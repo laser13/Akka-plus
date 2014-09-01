f2dfd0e8032e269991b2e3aed1709eaae577bd32
Pavlenov Semen <me@pavlenov.ru>
2014-09-01 05:58:07 +0400

    ===

--- src/main/scala/ru/pavlenov/Main.scala
+++ src/main/scala/ru/pavlenov/Main.scala
@@ -3,11 +3,6 @@ package ru.pavlenov
 /**
  * ⓭ + 15
  * Какой сам? by Pavlenov Semen 24.07.14.
- * ${TITLE}
- * ${URL}
- *
- * ${GIVEN}
- * ${RETURN}
  */
 
 import akka.actor._
@@ -15,6 +10,7 @@ import org.mashupbots.socko.events.HttpResponseStatus
 import org.mashupbots.socko.infrastructure.Logger
 import org.mashupbots.socko.routes._
 import org.mashupbots.socko.webserver.{WebServer, WebServerConfig}
+import ru.pavlenov.handler.{TimeRequest, TimeHandler}
 
 /**
  * This example shows how to setup a simple route and create a simple processor actor.
@@ -24,21 +20,21 @@ import org.mashupbots.socko.webserver.{WebServer, WebServerConfig}
  * Socko uses Netty to handle incoming requests and Akka to process them
  *  - Incoming requests are converted into Socko events using threads from the Netty thread pool
  *  - Your `routes` are then called to dispatch the event for processing
- *  - Inside our route definition, we instance a new `ru.pavlenov.HelloHandler` actor and pass the event to it
- *  - The `ru.pavlenov.HelloHandler` actor is executed in Akka default thread pool. This frees up the Netty thread pool to
+ *  - Inside our route definition, we instance a new `ru.pavlenov.handler.HelloHandler` actor and pass the event to it
+ *  - The `ru.pavlenov.handler.HelloHandler` actor is executed in Akka default thread pool. This frees up the Netty thread pool to
  *    undertake more networking activities.
  */
 object Main extends Logger {
   //
   // STEP #1 - Define Actors and Start Akka
-  // See `ru.pavlenov.HelloHandler`
+  // See `ru.pavlenov.handler.HelloHandler`
   //
   val actorSystem = ActorSystem("HelloExampleActorSystem")
 
   //
   // STEP #2 - Define Routes
-  // Dispatch all HTTP GET events to a newly instanced `ru.pavlenov.HelloHandler` actor for processing.
-  // `ru.pavlenov.HelloHandler` will `stop()` itself after processing each request.
+  // Dispatch all HTTP GET events to a newly instanced `ru.pavlenov.handler.HelloHandler` actor for processing.
+  // `ru.pavlenov.handler.HelloHandler` will `stop()` itself after processing each request.
   //
   val routes = Routes({
 //    case GET(request) =>
@@ -77,28 +73,23 @@ object Main extends Logger {
   //
   def main(args: Array[String]) {
 
-    val myWebServerConfig1 = WebServerConfig(
-      serverName = "CodedConfigApp",
-      hostname = "localhost",
-      port = 3001)
+//    val myWebServerConfig1 = WebServerConfig(
+//      serverName = "CodedConfigApp",
+//      hostname = "localhost",
+//      port = 3001)
 
-    log.info("Loading configuration from application.conf")
-    val myWebServerConfig2 = MyWebServerConfig(actorSystem)
-    log.info("Config is: " + myWebServerConfig2.toString)
+//    log.info("Loading configuration from application.conf")
+//    val serverConfig = ServerConfig(actorSystem)
+//    log.info("Config is: " + serverConfig.toString)
+//
+//    val webServer = new WebServer(serverConfig, routes, actorSystem)
+//    webServer.start()
+//    println(serverConfig.hostname, serverConfig.port)
 
-    val webServer = new WebServer(myWebServerConfig2, routes, actorSystem)
-    webServer.start()
-    println(myWebServerConfig2.hostname, myWebServerConfig2.port)
-
-    Runtime.getRuntime.addShutdownHook(new Thread {
-      override def run() { webServer.stop() }
-    })
+//    Runtime.getRuntime.addShutdownHook(new Thread {
+//      override def run() { webServer.stop() }
+//    })
 
   }
 }
 
-object MyWebServerConfig extends ExtensionId[WebServerConfig] with ExtensionIdProvider {
-  override def lookup() = MyWebServerConfig
-  override def createExtension(system: ExtendedActorSystem) =
-    new WebServerConfig(system.settings.config, "akka-config-example")
-}
--- src/main/scala/ru/pavlenov/ServerConfig.scala
+++ src/main/scala/ru/pavlenov/ServerConfig.scala
@@ -1,9 +1,14 @@
 package ru.pavlenov
 
+import akka.actor.{ExtendedActorSystem, ExtensionIdProvider, ExtensionId}
+import org.mashupbots.socko.webserver.WebServerConfig
+
 /**
  * ⓭ + 07
  * Какой сам? by Pavlenov Semen 26.07.14.
  */
-object ServerConfig {
-
+object ServerConfig extends ExtensionId[WebServerConfig] with ExtensionIdProvider {
+    override def lookup() = ServerConfig
+    override def createExtension(system: ExtendedActorSystem) =
+      new WebServerConfig(system.settings.config, "akka-config-example")
 }
